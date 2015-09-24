a1e815001bcc2b27cfb75873962354070598f624
theoldnewbie <c00per@mail.ua>
2015-09-24 20:43:15 +0300

    test

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 var headUrl = 'http://theoldnewbie.github.io/ajax_v1.01/';
 var htmlList = ['simple1.html','simple2.html','simple3.html'];
+var jsonList = ['some1.json','some2.json','some3.json'];
+var imgList = ['img/Troll1.jpg','img/Troll2.jpg','img/Troll3.jpg'];
 
 function loadHtmlFiles (element) {
 	$.ajax({
@@ -13,4 +15,17 @@ function loadHtmlFiles (element) {
 	});
 }
 
+function loadImgFiles (element) {
+	$.ajax({
+		url: headUrl + element,
+		success: function(data) {
+			$('.main-content').append('<img src = ' + this.url + '>');
+		}
+	});
+}
+
 htmlList.forEach( loadHtmlFiles );
+
+imgList.forEach( loadImgFiles );
+
+
