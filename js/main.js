18d08ba33aa3c0aaf24a18f9011d2604c8af5324
theoldnewbie <c00per@mail.ua>
2015-09-24 19:52:12 +0300

    test

--- js/main.js
+++ js/main.js
@@ -1,93 +1,16 @@
-$('button').on('click', function(){
-		
+var headUrl = 'http://theoldnewbie.github.io/ajax_v1.01/';
+var htmlList = ['simple1.html','simple2.html','simple3.html'];
+
+function loadHtmlFiles (element) {
 	$.ajax({
-			url: 'http://theoldnewbie.github.io/ajax_v1.01/simple1.html',
-			success: function(data) {
-				$('#1').html(data);
-			},
-			error: function(data) {
-	  			console.error(data);
-			}
+		url: headUrl + element,
+		success: function(data) {
+			$('.main-content').append(data);
+		},
+		error: function(data) {
+			console.error(data);
+		}
 	});
+}
 
-	$.ajax({
-			url: 'http://theoldnewbie.github.io/ajax_v1.01/simple2.html',
-			success: function(data) {
-				$('#2').html(data);
-			},
-			error: function(data) {
-				console.error(data);
-			}
-	});
-
-	$.ajax({
-			url: 'http://theoldnewbie.github.io/ajax_v1.01/simple3.html',
-			success: function(data) {
-				$('#3').html(data);
-			},
-			error: function(data) {
-				console.error(data);
-			}
-	});
-
-	// $.ajax({
-	// 		url: 'http://theoldnewbie.github.io/ajax_v1.01/some1.json',
-	// 		success: function(data) {
-	// 			document.body.appendChild(data);
-	// 		},
-	// 		error: function(data) {
-	// 			console.error(data);
-	// 		}
-	// });
-
-	// $.ajax({
-	// 		url: 'http://theoldnewbie.github.io/ajax_v1.01/some2.json',
-	// 		success: function(data) {
-	// 			document.body.appendChild(data);
-	// 		},
-	// 		error: function(data) {
-	// 			console.error(data);
-	// }
-	// });
-
-	// $.ajax({
-	// 		url: 'http://theoldnewbie.github.io/ajax_v1.01/some3.json',
-	// 		success: function(data) {
-	// 			document.body.appendChild(data);
-	// 		},
-	// 		error: function(data) {
-	// 			console.error(data);
-	// }
-	// });
-
-	// $.ajax({
-	// 		url: 'http://theoldnewbie.github.io/ajax_v1.01/img/Troll1.jpeg',
-	// 		success: function(data) {
-	// 			document.body.appendChild(data);
-	// 		},
-	// 		error: function(data) {
-	// 			console.error(data);
-	// 		}
-	// });
-
-	// $.ajax({
-	// 		url: 'http://theoldnewbie.github.io/ajax_v1.01/img/Troll2.jpeg',
-	// 		success: function(data) {
-	// 			document.body.appendChild(data);
-	// 		},
-	// 		error: function(data) {
-	// 		 	console.error(data);
-	// 		}
-	// });
-
-	
-	// $.ajax({
-	// 		url: 'http://theoldnewbie.github.io/ajax_v1.01/img/Troll3.jpeg',
-	// 		success: function(data) {
-	// 			document.body.appendChildren(data);
-	// 		},
-	// 		error: function(data) {
-	//  			console.error(data);
-	// 		}
-	// });	
-})
+htmlList.forEach( loadHtmlFiles );
