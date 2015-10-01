5a8cdf9de891b80d513ba3bf5be2b8d591e3ca3e
ArtemSidun <tomas1772@gmail.com>
2015-10-01 17:16:18 +0300

    test

--- js/main.js
+++ js/main.js
@@ -2,15 +2,27 @@ var headUrl = 'http://theoldnewbie.github.io/ajax_v1.01/';
 var htmlList = ['simple1.html','simple2.html','simple3.html'];
 var jsonList = ['some1.json','some2.json','some3.json'];
 var imgList = ['img/Troll1.jpg','img/Troll2.jpg','img/Troll3.jpg'];
+var isLoaded = {
+	b1: false,
+	b2: false,
+	b3: false
+}
 
 $('#1').on('click', function() {
+	if(!isLoaded.b1){
 	htmlList.forEach( loadHtmlFiles );
+	}
 });
 $('#2').on('click', function() {
+	if(!isLoaded.b2){
 	jsonList.forEach( loadJSONFiles );
+	}
 });
 $('#3').on('click', function() {
-	imgList.forEach( loadImgFiles );
+	if(!isLoaded.b3){
+		imgList.forEach( loadImgFiles );
+	}
+	
 });
 
 
@@ -20,6 +32,7 @@ function loadHtmlFiles (element) {
 		async: false,
 		success: function(data) {
 			$('.main-content').append(data);
+			isLoaded.b1 = true;
 		},
 		error: function(data) {
 			console.error(data);
@@ -37,6 +50,7 @@ function loadJSONFiles (element) {
      var wrapDiv = $('<div>');
      wrapDiv.html('<h2>' + data.title + '</h2><p>' + data.ty + '</p>');
      $('.main-content').append(wrapDiv);
+     isLoaded.b2 =true;
     },
     error: function(data) {
      console.error(data);
@@ -52,6 +66,7 @@ function loadImgFiles (element) {
 		async: false,
 		success: function(data) {
 			$('.main-content').append('<img src = ' + this.url + '>');
+			isLoaded.b3 =true;
 		}
 	});
 }
